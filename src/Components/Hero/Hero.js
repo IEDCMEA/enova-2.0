8d64dfb280639a837effa6668be1c4d2f51f03de
MohamedDanis <danishmanu17@gmail.com>
2022-04-12 13:13:03 +0530

    Added landing page

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -4,28 +4,28 @@ import './Hero.css'
 function Hero() {
   return (
     <div className="bg-white dark:bg-gray-800 overflow-hidden relative lg:flex lg:items-center hero">
-        <div className="w-full py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20 mt-40">
+        <div className="w-full py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20 hero-content">
           <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
-            <span className="block title text-glow font-righteous font-black text-4xl lg:text-6xl">
+            <span className="block title text-glow font-righteous font-black text-4xl lg:text-6xl text-center">
               ENOVA 2.0 is here
             </span>
           </h2>
-          <p className="text-lg mt-4 text-gray-600 font-tajawal">
+          <p className="text-lg mt-4 text-gray-600 font-tajawal lg:px-10">
             Hello folks, The most awaited event ENOVA 2.0, three day national level program is here with vibrant workshops, talks and cultural programs and more.<br/> You are cordially invited to participate in this event.
           </p>
-          <div className="lg:mt-0 lg:flex-shrink-0">
-            <div className="mt-12 inline-flex rounded-md shadow">
-              <button type="button" className="py-2 px-4   text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md   rounded-lg btn btn-gradient btn-glow ">
+          <div className="lg:mt-0 lg:flex-shrink-0 flex justify-center">
+            <div className="mt-12 inline-flex rounded-md shadow ">
+              <button type="button" className="py-2 px-4   text-white w-48 h-14 transition ease-in duration-200 text-center text-lg font-semibold shadow-md   rounded-lg btn btn-gradient btn-glow ">
                 Register Now
               </button>
             </div>
           </div>
         </div>
-        <div className="flex items-center gap-8 p-8 lg:p-24 lg:mt-40">
-          <img src="https://images.unsplash.com/photo-1517263904808-5dc91e3e7044?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" className="rounded-lg w-1/2" alt="Tree" />
+        <div className="flex items-center gap-8 p-4  lg:p-24 lg:mt-40 w-full lg:mt-80 img-container">
+          <img src="https://i.ibb.co/FscFZv3/IMG-7438.jpg" className="rounded-lg w-1/2" alt="Tree" />
           <div>
-            <img src="https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" className="rounded-lg mb-8" alt="Tree" />
-            <img src="https://images.unsplash.com/photo-1522158637959-30385a09e0da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="rounded-lg" alt="Tree" />
+            <img src="https://i.ibb.co/TWdnpG3/IMG-0964.jpg" className="rounded-lg mb-8 " alt="Tree" />
+            <img src="https://i.ibb.co/60RvPDG/IMG-8192.jpg" className="rounded-lg " alt="Tree" />
           </div>
         </div>
       </div>
